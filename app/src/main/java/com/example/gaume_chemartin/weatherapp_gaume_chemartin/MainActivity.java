6502cc9b544c2dca2f63117259cc01a2acf61ed8
ZekLouis <zeklouis@gmail.com>
2017-02-27 17:46:10 +0100

    First Commit

---
+++ app/src/main/java/com/example/gaume_chemartin/weatherapp_gaume_chemartin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gaume_chemartin.weatherapp_gaume_chemartin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
